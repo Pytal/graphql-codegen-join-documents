e6b86b50e425c7acc59779080e5cb806c05b876c
Chris <chrng2@gmail.com>
2020-07-01 21:13:59 -0700

    refactor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const { print } = require('graphql')
 
 module.exports = {
   plugin: (schema, documents, config, info) => (
-    documents.map(({ document }) => print(document)).join('\n')
+    documents.map( ({ document }) => print(document) ).join('\n')
   )
 }
